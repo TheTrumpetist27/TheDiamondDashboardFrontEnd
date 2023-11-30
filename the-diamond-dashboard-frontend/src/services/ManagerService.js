f522f041ad277b79158f075d7d369c7cf68d4e7c
Kian Bruyenberg <kbruyenberg@gmail.com>
2023-11-30 15:09:02 +0100

    Updated api link to docker

--- the-diamond-dashboard-frontend/src/services/ManagerService.js
+++ the-diamond-dashboard-frontend/src/services/ManagerService.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://localhost:7078/api/Manager';
+const API_URL = 'https://localhost:32776/api/Manager';
 
 export const getManagerList = async () => {
     const response = await fetch(API_URL);
